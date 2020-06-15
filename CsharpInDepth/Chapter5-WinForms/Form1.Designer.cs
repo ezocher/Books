06694d39ed2717cfdb1375815deee8cf510ba935
Eric Zocher <ezocher@users.noreply.github.com>
2020-06-14 21:08:08 -0700

    Initial check-in
    WinForms app (async/await) from Chapter 5 of C# In Depth 4e

---
+++ CsharpInDepth/Chapter5-WinForms/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Chapter5_WinForms
+{
+    partial class AsyncIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonLoadPage = new System.Windows.Forms.Button();
+            this.richTextBoxPageContents = new System.Windows.Forms.RichTextBox();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.checkBoxHEADRequest = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(108, 40);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 0;
+            // 
+            // buttonLoadPage
+            // 
+            this.buttonLoadPage.Location = new System.Drawing.Point(16, 35);
+            this.buttonLoadPage.Name = "buttonLoadPage";
+            this.buttonLoadPage.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadPage.TabIndex = 1;
+            this.buttonLoadPage.Text = "Load Page";
+            this.buttonLoadPage.UseVisualStyleBackColor = true;
+            this.buttonLoadPage.Click += new System.EventHandler(this.DisplayWebPage);
+            // 
+            // richTextBoxPageContents
+            // 
+            this.richTextBoxPageContents.Location = new System.Drawing.Point(16, 74);
+            this.richTextBoxPageContents.Name = "richTextBoxPageContents";
+            this.richTextBoxPageContents.Size = new System.Drawing.Size(1172, 814);
+            this.richTextBoxPageContents.TabIndex = 2;
+            this.richTextBoxPageContents.Text = "";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(16, 9);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(496, 20);
+            this.textBoxUrl.TabIndex = 3;
+            // 
+            // checkBoxHEADRequest
+            // 
+            this.checkBoxHEADRequest.AutoSize = true;
+            this.checkBoxHEADRequest.Location = new System.Drawing.Point(555, 12);
+            this.checkBoxHEADRequest.Name = "checkBoxHEADRequest";
+            this.checkBoxHEADRequest.Size = new System.Drawing.Size(94, 17);
+            this.checkBoxHEADRequest.TabIndex = 4;
+            this.checkBoxHEADRequest.Text = "HEAD request";
+            this.checkBoxHEADRequest.UseVisualStyleBackColor = true;
+            // 
+            // AsyncIntro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 900);
+            this.Controls.Add(this.checkBoxHEADRequest);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.richTextBoxPageContents);
+            this.Controls.Add(this.buttonLoadPage);
+            this.Controls.Add(this.labelStatus);
+            this.Name = "AsyncIntro";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonLoadPage;
+        private System.Windows.Forms.RichTextBox richTextBoxPageContents;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.CheckBox checkBoxHEADRequest;
+    }
+}
+
---
+++ CsharpInDepth/Chapter5-WinForms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter5_WinForms
+{
+    public partial class AsyncIntro : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private const string DefaultUrl = "http://csharpindepth.com";
+
+        public AsyncIntro()
+        {
+            InitializeComponent();
+            textBoxUrl.Text = DefaultUrl;
+            textBoxUrl.KeyDown += TextBox1_KeyDown;
+
+            SetUserAgent();
+        }
+
+        private void SetUserAgent()
+        {
+            //Add a user-agent header to the GET request. 
+            var headers = client.DefaultRequestHeaders;
+
+            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
+            //especially if the header value is coming from user input.
+            string header = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;          // Prevents beep when Enter pressed
+                DisplayWebPage(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private async void DisplayWebPage(object sender, EventArgs e)
+        {
+            Stopwatch elapsedTime = new Stopwatch();
+            string text;
+
+            string url = textBoxUrl.Text;
+            if (url == "") url = DefaultUrl;
+
+            labelStatus.Text = "Fetching...";
+            elapsedTime.Start();
+
+            try
+            {
+                string source;
+
+                if (checkBoxHEADRequest.Checked)
+                {
+                    text = (await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url))).ToString();
+                    source = "HEAD of " + url;
+                }
+                else
+                {
+                    text = await client.GetStringAsync(url);
+                    source = url;
+                }
+
+                long timeHttp = elapsedTime.ElapsedMilliseconds;
+                richTextBoxPageContents.Text = text;
+                labelStatus.Text = String.Format("Length of {0}: {1:N0} chars in http: {2:N0} / total: {3:N0} ms", source, text.Length, timeHttp, elapsedTime.ElapsedMilliseconds);
+            }
+            catch (Exception ex)
+            {
+                string details;
+                labelStatus.Text = "Exception";
+                if (ex.InnerException == null)
+                    details = "";
+                else
+                    details = ex.InnerException.Message;
+                richTextBoxPageContents.Text = String.Format("{0}\n{1}\nElapsed time: {2:N0} ms", ex.Message, details, elapsedTime.ElapsedMilliseconds);
+            }
+        }
+    }
+}

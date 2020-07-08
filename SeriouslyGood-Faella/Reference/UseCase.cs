95b24184c9a003845421831b068bdfd60d43978c
Eric Zocher <ezocher@users.noreply.github.com>
2020-07-07 22:44:27 -0700

    Renaming in UseCase class

--- SeriouslyGood-Faella/Reference/UseCase.cs
+++ SeriouslyGood-Faella/Reference/UseCase.cs
@@ -12,21 +12,21 @@ namespace Reference
             Container b = new Container();
             Container c = new Container(); 
             Container d = new Container();
-            PrintContainers("Part 1: ", a, b, c, d);
+            DisplayContainers("Part 1: ", a, b, c, d);
 
             a.addOrRemoveWater(12);
             d.addOrRemoveWater(8);
             a.connectTo(b);
-            PrintContainers("Part 2: ", a, b, c, d);
+            DisplayContainers("Part 2: ", a, b, c, d);
 
             b.connectTo(c);
-            PrintContainers("Part 3: ", a, b, c, d);
+            DisplayContainers("Part 3: ", a, b, c, d);
 
             b.connectTo(d);
-            PrintContainers("Part 4: ", a, b, c, d);
+            DisplayContainers("Part 4: ", a, b, c, d);
         }
 
-        public static void PrintContainers(string label, params Container[] containers)
+        public static void DisplayContainers(string label, params Container[] containers)
         {
             Console.Write(label);
             foreach (Container container in containers)

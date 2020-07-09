5d148b89fc562c53bbe484c87d26b9842db70462
Eric Zocher <ezocher@users.noreply.github.com>
2020-07-08 18:54:00 -0700

    Added progress messages for every operation
    Created static DIsplay class; Added name field to each container

--- SeriouslyGood-Faella/Reference/UseCase.cs
+++ SeriouslyGood-Faella/Reference/UseCase.cs
@@ -8,60 +8,54 @@ namespace Reference
     {
         public static void RunUseCase()
         {
-            Container a = new Container();
-            Container b = new Container();
-            Container c = new Container(); 
-            Container d = new Container();
-            DisplayContainers("Part 1: ", a, b, c, d);
+            Container a = new Container("a");
+            Container b = new Container("b");
+            Container c = new Container("c"); 
+            Container d = new Container("d");
+            Display.Containers("Part 1: ", a, b, c, d);
 
             a.addOrRemoveWater(12);
             d.addOrRemoveWater(8);
             a.connectTo(b);
-            DisplayContainers("Part 2: ", a, b, c, d);
+            Display.Containers("Part 2: ", a, b, c, d);
 
             b.connectTo(c);
-            DisplayContainers("Part 3: ", a, b, c, d);
+            Display.Containers("Part 3: ", a, b, c, d);
 
             b.connectTo(d);
-            DisplayContainers("Part 4: ", a, b, c, d);
+            Display.Containers("Part 4: ", a, b, c, d);
         }
 
         public static void RunUseCaseMultipleConnections()
         {
             Container a = new Container(), b = new Container(), c = new Container();
             Container d = new Container(), e = new Container(), f = new Container();
-            DisplayContainers("Step 1: ", a, b, c, d, e, f);
+            Display.Containers("Step 1: ", a, b, c, d, e, f);
 
             a.addOrRemoveWater(12);
             d.addOrRemoveWater(18);
-            DisplayContainers("Step 2: ", a, b, c, d, e, f);
+            Display.Containers("Step 2: ", a, b, c, d, e, f);
 
             a.connectTo(b);
             b.connectTo(c);
             d.connectTo(e);
-            DisplayContainers("Step 3: ", a, b, c, d, e, f);
+            Display.Containers("Step 3: ", a, b, c, d, e, f);
 
             e.connectTo(d);
             c.connectTo(a);
-            DisplayContainers("Step 4, multiple connections, should == Step 3: ", a, b, c, d, e, f);
+            Display.Containers("Step 4, multiple connections, should == Step 3: ", a, b, c, d, e, f);
 
             f.connectTo(b);
-            DisplayContainers("Step 5: ", a, b, c, d, e, f);
+            Display.Containers("Step 5: ", a, b, c, d, e, f);
 
             d.connectTo(f);
-            DisplayContainers("Step 6: ", a, b, c, d, e, f);
+            Display.Containers("Step 6: ", a, b, c, d, e, f);
 
             a.connectTo(e);
             a.connectTo(a);
-            DisplayContainers("Step 7, multiple connections, should == Step 6: ", a, b, c, d, e, f);
+            Display.Containers("Step 7, multiple connections, should == Step 6: ", a, b, c, d, e, f);
         }
 
-        public static void DisplayContainers(string label, params Container[] containers)
-        {
-            Console.Write(label);
-            foreach (Container container in containers)
-                Console.Write(container.getAmount() + "  ");
-            Console.WriteLine();
-        }
+
     }
 }
